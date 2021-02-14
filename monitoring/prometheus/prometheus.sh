88bf616a165aadc1872a973b31cd72d733da5cab
nchern <nchern@inbox.ru>
2021-02-14 19:25:10 +0100

    monitoring: add prometheus skeleton

---
+++ monitoring/prometheus/prometheus.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ue
+
+
+VER="v2.22.0"
+PROM_VOLUME="prom_local"
+
+docker run -ti \
+	--network="host" \
+	--name="prometheus" \
+	-v "$PROM_VOLUME":/prometheus \
+	-v /etc/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:ro \
+	prom/prometheus:"$VER" \
+	    --web.listen-address=localhost:9090 \
+        --config.file=/etc/prometheus/prometheus.yml \
+        --storage.tsdb.path=/prometheus
