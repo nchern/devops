04de2e716cc9406ecfcfb5f1269786b5550262d1
nchern <nchern@inbox.ru>
2022-01-09 21:58:36 +0100

    prometheus: add init command to the script

--- monitoring/prometheus/prometheus.sh
+++ monitoring/prometheus/prometheus.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 set -ue
 
-VER="v2.22.0"
-PROM_VOLUME="prom_local"
+VER="v2.32.1"
+IMAGE="prom/prometheus:$VER"
 
+VOLUME="prom_local"
+
+
+init() {
+    # docker volume create $VOLUME
+    docker pull "$IMAGE"
+}
+
+
+case "${1:-}" in
+	"init")
+		init && exit 0
+		;;
+esac
 
 exec docker run -ti --rm \
 	--network="host" \
 	--name="prometheus" \
-	-v "$PROM_VOLUME":/prometheus \
+	-v "$VOLUME:/prometheus" \
 	-v /etc/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:ro \
-	prom/prometheus:"$VER" \
+	"$IMAGE" \
         --web.listen-address=localhost:9090 \
         --config.file=/etc/prometheus/prometheus.yml \
         --storage.tsdb.path=/prometheus
