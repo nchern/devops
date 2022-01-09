f5e0bc5fcda69c889b1e77db3e0a0890ccfe8cac
nchern <nchern@inbox.ru>
2022-01-09 21:22:08 +0100

    prometheus: add README; extract install-systemd target

---
+++ monitoring/prometheus/README.md
@@ -0,0 +1,3 @@
+Runs prometheus as a systemd-service in a docker container.
+It runs under the user `nobody` - as designed by the prom's docker image.
+Prometheus version is set in `prometheus.sh`
--- monitoring/prometheus/prometheus.sh
+++ monitoring/prometheus/prometheus.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -ue
 
-
 VER="v2.22.0"
 PROM_VOLUME="prom_local"
+
 
 exec docker run -ti --rm \
 	--network="host" \
@@ -11,6 +11,6 @@ exec docker run -ti --rm \
 	-v "$PROM_VOLUME":/prometheus \
 	-v /etc/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml:ro \
 	prom/prometheus:"$VER" \
-	    --web.listen-address=localhost:9090 \
+        --web.listen-address=localhost:9090 \
         --config.file=/etc/prometheus/prometheus.yml \
         --storage.tsdb.path=/prometheus
