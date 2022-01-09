68f22b7ed5693f4bc72dcc6fc12330ac196fe8de
nchern <nchern@inbox.ru>
2022-01-09 17:30:39 +0100

    pam-login-notify: refactoring
    - move installation logic to makefile
    - make notification script more straigntforward and explained

--- notifications/pam-login-notify/init.sh
+++ notifications/pam-login-notify/init.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 set -ue
 
-BIN_DIR="/usr/local/bin"
-NOTIFY="pam-login-notify.sh"
-TARGET="$BIN_DIR/$NOTIFY"
+TARGET="$1"
 
 echo -n "Enter email address to be notified upon login [ENTER]: "
 read -r EMAIL
 
-install -c "$NOTIFY" "$BIN_DIR/"
-
-chown root:root "$TARGET"
-
 sed -i s"/%EMAIL%/$EMAIL/g" "$TARGET"
 
-echo "session optional pam_exec.so seteuid $BIN_DIR/$NOTIFY" >> /etc/pam.d/sshd
+echo "session optional pam_exec.so seteuid $TARGET" >> /etc/pam.d/sshd
--- notifications/pam-login-notify/pam-login-notify.sh
+++ notifications/pam-login-notify/pam-login-notify.sh
@@ -12,28 +12,33 @@ SILENT_INTERVAL_SEC=60  # do not send notification if subsequent accesses happen
 
 LAST_ACCESSED_FILE="/home/$PAM_USER/.last_ssh_accessed"
 
+KNOWN_REMOTES_FILE="/home/$PAM_USER/.known_remotes"
+
+if [ "$PAM_TYPE" = "close_session" ]; then
+    # do nothing if session is being closed
+    exit 0
+fi
+
+# continue with starting a new session
+
+if [ -f "$KNOWN_REMOTES_FILE" ] && grep -q "$PAM_RHOST" "$KNOWN_REMOTES_FILE" ; then
+    # if we know this remote, don't send anything
+    exit 0
+fi
+
 LAST_ACCESSED=$(stat -c %Y "$LAST_ACCESSED_FILE" 2> /dev/null || echo 0)
 
 NOW=$(date +'%s')
 if [ $((NOW-LAST_ACCESSED)) -le "$SILENT_INTERVAL_SEC" ]; then
+    # don't spam if multiple connections from the same user happen within silent interval
     exit 0
 fi
 
-
 HOST=$(hostname)
 
-KNOWN_REMOTES_FILE="/home/$PAM_USER/.known_remotes"
+SUBJECT="SSH Login[$HOST]: $PAM_USER from $PAM_RHOST"
+MESSAGE="Successful login using $PAM_SERVICE"
 
-if [ "$PAM_TYPE" != "close_session" ]; then
-    if [ -f "$KNOWN_REMOTES_FILE" ] && grep -q "$PAM_RHOST" "$KNOWN_REMOTES_FILE" ; then
-        # if we know this remote, don't send anything
-    	exit 0
-    fi
+echo "$MESSAGE" | mail -s "$SUBJECT" "$TO"
 
-    SUBJECT="SSH Login[$HOST]: $PAM_USER from $PAM_RHOST"
-    MESSAGE="Successful login using $PAM_SERVICE"
-
-    echo "$MESSAGE" | mail -s "$SUBJECT" "$TO"
-
-    touch "$LAST_ACCESSED_FILE"
-fi
+touch "$LAST_ACCESSED_FILE"
