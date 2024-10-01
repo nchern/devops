b25611a18fca86299661c46c6bdb63c083a39288
nchern <nchern@inbox.ru>
2024-10-01 23:54:09 +0200

    bootstrap-node.sh: bugfixes; refactor

--- bootstrap-node.sh
+++ bootstrap-node.sh
@@ -4,21 +4,20 @@ set -ue
 # Credits to Drew DeVault(https://drewdevault.com/)
 # Inspired by and created after his https://drewdevault.com/new-server.html
 
+RED='\033[1;31m'    # Ligt red
+NC='\033[0m'        # No Color
 
 # Requires at least Debian-like
 # Tested on Ubuntu(18.04 / 20.04)
 if ! grep -q 'ID_LIKE=debian' /etc/os-release; then
-    echo "The system does not look like Debian and the likes. Halt."
-    grep --with-filename -E '(NAME|ID)' /etc/os-release
+    echo "/etc/os-release:"
+    grep -E '(NAME|ID)' /etc/os-release || true
+    echo
+    echo "fatal: The system does not look like Debian and the likes. Halt."
     exit 1
 fi
 
-RED='\033[1;31m'    # Ligt red
-NC='\033[0m'        # No Color
-
-password() {
-    openssl rand -base64 24
-}
+password() { openssl rand -base64 24 ; }
 
 
 apt-get -y update && 
@@ -33,10 +32,13 @@ apt-get -y update &&
         ssmtp           \
         net-tools       \
         moreutils       \
+        sudo            \
         openssl
 
-echo "Generating key pair for user root:" && cat /dev/zero | ssh-keygen -q -N "" -t rsa
-echo ""
+alias ssh-keygen='ssh-keygen -t ed25519 -q'
+
+echo "Generating key pair for user root:" && ssh-keygen -N "" < /dev/zero
+echo
 
 # set root password
 root_passwd=$(password)
@@ -45,30 +47,32 @@ echo "root:$root_passwd" | chpasswd  # ubuntu only
 # Create sudoer admin
 admin_user="watcher"
 echo -n "Enter admin user name [${admin_user}]: "
-read -r admin_user
+read -r name
+[ "$name" != "" ] && admin_user=$name
 
-admin_user_ssh_dir="/home/$admin_user/.ssh/"
 adduser --gecos "" --disabled-password "$admin_user" && usermod -aG sudo "$admin_user"
 # Generate key pair for admin user
-echo "Generating key pair for user $admin_user:" && cat /dev/zero | sudo -u "$admin_user" ssh-keygen -q -N "" -t ed25519
-echo ""
+echo "Generating key pair for user $admin_user:" && cat /dev/zero | sudo -u "$admin_user" ssh-keygen -N ""
+echo
 
 admin_passwd=$(password)
 echo "$admin_user:$admin_passwd" | chpasswd  # ubuntu only
+admin_user_ssh_dir="/home/$admin_user/.ssh/"
 cp ~/.ssh/authorized_keys "$admin_user_ssh_dir" &&
     chown -R "$admin_user:$admin_user" "$admin_user_ssh_dir" &&
     chmod 0600 "$admin_user_ssh_dir/authorized_keys"
 
-echo "root password: ${RED}$root_passwd${NC}"
-echo "$admin_user password: ${RED}$admin_passwd${NC}"
+printf "root password: ${RED}$root_passwd${NC}\n"
+printf "$admin_user password: ${RED}$admin_passwd${NC}\n"
 
-# tweak ssh accesses
+# harden ssh accesses
 sshd_config="/etc/ssh/sshd_config"
 cp $sshd_config $sshd_config.original
 
 sed s'/#PermitEmpty/PermitEmpty/g' -i.orig $sshd_config
 sed s'/#PasswordAuthentication yes/PasswordAuthentication no/g' -i $sshd_config
-sed s'/PermitRootLogin yes/PermitRootLogin no/g' -i $sshd_config
+sed s'/#PermitRootLogin .*$/PermitRootLogin no/g' -i $sshd_config
+echo
 
 echo "Checkout https://drewdevault.com/new-server.html"
 echo "Carefully check $sshd_config and then run"
